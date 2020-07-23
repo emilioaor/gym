f8f0a51a8bf8a0676192f83c4a4e85cdd8b61a81
Emilio Ochoa <eochoa@navicu.com>
2020-07-23 17:37:20 -0400

    Alerta 5 dias antes de expiracion

--- application/controllers/Member_controller.php
+++ application/controllers/Member_controller.php
@@ -7,6 +7,15 @@ class Member_controller extends CI_Controller {
 	{
 		$data['main_view'] = "member_view";
 
+        $now = new \DateTime();
+        $expDate = \DateTime::createFromFormat('Y-m-d', $this->session->userdata('member')->member_exp_date);
+        $before5 = \DateTime::createFromFormat('Y-m-d', $this->session->userdata('member')->member_exp_date)->modify('-5 days');
+
+        if ($now >= $before5) {
+            $this->session->set_flashdata('alert_type', 'danger');
+            $this->session->set_flashdata('alert_msg', 'Your expiration date is ' . $expDate->format('d M') . '. Please renew your subscription');
+        }
+
 		$this->load->view('layouts/main', $data);
 	}
 }
